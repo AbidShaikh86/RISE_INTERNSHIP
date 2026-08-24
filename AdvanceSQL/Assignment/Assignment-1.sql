773754b065f621eeccea69906096735d0550e945
AbidShaikh86 <abidshaikh.86@yahoo.com>
2026-08-24 10:16:37 +0530

    Alter: Assignment-1

--- AdvanceSQL/Assignment/Assignment-1.sql
+++ AdvanceSQL/Assignment/Assignment-1.sql
@@ -38,7 +38,7 @@ WHERE Ename = @EID
 END
 
 --4. Create a stored procedure to insert a new department record into the Department table by passing DeptID, DeptName, and Location as parameters.
-CREATE PROC sp_InsertInDep_byDeptID_DeptName_Location
+CREATE PROC sp_InsertDepartmentbyId
 @DeptID INT,
 @DeptName VARCHAR(30)
 AS
@@ -47,7 +47,7 @@ INSERT INTO department_info(DID, Dname) VALUES (@DeptID, @DeptName)
 end
 
 --5. Create a stored procedure that takes an EID and a NewSalary as parameters to update an employee's salary in the Employee table.
-CREATE PROC sp_UpdateSal_byEID_NewSal
+CREATE PROC sp_UpdateSalaryByPer
 @EmployeeID INT,
 @NewSalary DECIMAL(12,2)
 AS
@@ -58,7 +58,7 @@ WHERE EID = @EmployeeID
 END
 
 --6. Create a stored procedure that takes a search string parameter (e.g., 'a') and returns all employees whose names start with that letter.
-CREATE PROC sp_Search_byString
+CREATE PROC sp_SearchString
 @String VARCHAR(20)
 AS
 BEGIN
@@ -67,14 +67,14 @@ WHERE EName like @string + '%'
 END
 
 --7. Create a stored procedure that calculates and returns the average salary of all employees in the Employee table.
-CREATE PROC sp_AvgSal
+CREATE PROC sp_averageSalary
 AS
 BEGIN
 SELECT Avg(Esalary) AS AverageSalary FROM employee_info
 END
 
 --8. Create a stored procedure that takes two input parameters, MinSalary and MaxSalary, and returns all employees whose salary falls within that range.
-CREATE PROC sp_Salary_byMinSalary_MaxSalary
+CREATE PROC sp_maxSalary_minSalary
 @MinSalary INT,
 @MaxSalary INT
 AS
@@ -84,7 +84,7 @@ WHERE Esalary between @MinSalary and @MaxSalary
 END
 
 --9. Create a stored procedure that takes a ProjectID and a new Status string (e.g., 'Completed') as input parameters and updates the status of that project in the Project table.
-CREATE PROC sp_UpdatePrj_byPID_Status
+CREATE PROC sp_updateProjectStatus
 @ProjectID INT,
 @Status VARCHAR(50)
 as
@@ -95,7 +95,7 @@ WHERE PID = @ProjectID
 end
 
 --10.Create a stored procedure that takes a City / Location name as an input parameter and displays the total number of departments located in that city.
-CREATE PROC sp_Display_byCity
+CREATE PROC sp_departmentByCity
 @City VARCHAR(30)
 AS
 BEGIN
